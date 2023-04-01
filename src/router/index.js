e697e29ae63e00b346c401a672ced0adae674f53
aromero-at-858853655499 <aromero@bluepeople.com>
2023-03-31 18:43:49 -0600

    Crear resena

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import Create from '../views/Create.vue'
+
 
 Vue.use(VueRouter)
 
@@ -16,17 +18,15 @@ const routes = [
     component: AboutView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/create/:id',
+    name: 'create',
+    component: Create,
+    params:true
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
